d50d31223181b7ba372afdcb5e8c20ab9f5027ec
Vasiliy Shilov <shcoder.ru@ya.ru>
2016-12-23 11:23:15 +0300

    sleep, mouseEvent

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ const g = new GUIT({
 
 (async function() {
     await g.open('http://localhost/');
-    await g.render('test.png');
+    await g.render('tmp/test1.png');
+    await g.mouseEvent('click', 175, 344, 'left');
+    await g.render('tmp/test2.png');
+    await g.mouseEvent('click', 146, 86, 'left');
+    await g.render('tmp/test3.png');
+    await g.mouseEvent('click', 280, 341, 'left');
+    await g.sleep(1000); // animation
+    await g.render('tmp/test4.png');
 })();
