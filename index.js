fd3bcffbe0e3a5bfdc7b6738d57e4c7b5194096f
Vasiliy Shilov <shcoder.ru@ya.ru>
2016-12-25 23:38:28 +0300

    save,load,calculate snapshot

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 import GUIT from './lib/guit';
+import assert from 'assert';
 
 const g = new GUIT({
-    width: 320,
-    height: 480,
+    width: 1280,
+    height: 1024,
     checkTimeout: 100,
     doneTimeout: 5000
 });
 
 (async function() {
     await g.open('http://localhost/');
-    await g.render('tmp/test1.png');
-    await g.mouseEvent('click', 175, 344, 'left');
-    await g.render('tmp/test2.png');
-    await g.mouseEvent('click', 146, 86, 'left');
-    await g.render('tmp/test3.png');
-    await g.mouseEvent('click', 280, 341, 'left');
-    await g.sleep(1000); // animation
-    await g.render('tmp/test4.png');
+    // await g.render('example-screenshot.png');
+    let calculatedSnapshot = await g.getSnapshot();
+    // await g.saveSnapshot('example-snapshot.json', snapshot);
+    let savedSnapshot = await g.loadSnapshot('example-snapshot.json');
+    assert.deepEqual(calculatedSnapshot, savedSnapshot);
 })();
