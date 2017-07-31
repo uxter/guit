5a62c61d95384bd9244fb41be3b278d20ba44740
Vasiliy Shilov <shcoder.ru@ya.ru>
2017-07-31 14:22:09 +0300

    update username

--- src/guit.js
+++ src/guit.js
@@ -1,9 +1,9 @@
 /**
  *  @fileOverview   GUIT facade
  *  @description    The JavaScript framework for testing web UI.
- *  @author         Vasily Shilov <shcoder.ru@ya.ru> (https://github.com/shcoder-ru)
- *  @licence        ISC (https://github.com/shcoder-ru/guit/blob/master/LICENSE.txt)
- *  {@link          https://github.com/shcoder-ru/guit GitHub}
+ *  @author         Vasily Shilov <shcoder.ru@ya.ru> (https://github.com/uxter)
+ *  @licence        ISC (https://github.com/uxter/guit/blob/master/LICENSE.txt)
+ *  {@link          https://github.com/uxter/guit GitHub}
  *  {@link          https://www.npmjs.com/package/guit NPM}
  */
 
